80a00898e0a67d576f7a7d0de4ea735ed810a8f1
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-03-08 11:48:07 -0500

    Update basic.swift

--- Switf/basic.swift
+++ Switf/basic.swift
@@ -1,1 +1,24 @@
+var star = 10
+for counter in 1...star{
+    for i in 1...star-counter{
+        print(" ", terminator: "")
+    }
+    for l in 1...counter{
+        print("* ", terminator: "")
+    }
+    for x in 1...star-counter{
+        print("  ", terminator: "")
+    }
+    for k in 1...counter{
+        print("* ", terminator: "")
+    }
+    for y in 1...star-counter{
+        print("  ", terminator: "")
+    }
+    for j in 1...counter{
+        print("* ", terminator: "")
+    }
+    print("\n")
 
+    
+}
