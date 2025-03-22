677eb7c540f948d5768d52598fca54ea45f03181
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-03-21 20:19:44 -0400

    Update practice.swift

--- Switf/practice.swift
+++ Switf/practice.swift
@@ -1,6 +1,14 @@
 import SwiftUI
 
 struct ContenView: Vies {
+    VStack{
+        Circle()
+            .frame(width:150, height:15)
+            .foregroundColor(.orange)
+        Text("JD")
+            .font(.system(size:100))
+            .foregroundColor(.white)
+    }
     
 struct ContentView_Previews: PreviewProvider{
     static var previews: some View {
