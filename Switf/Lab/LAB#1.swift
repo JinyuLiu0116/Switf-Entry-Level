5253708024ccb6c445655f986fa2cff52d0c654d
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-03-22 10:57:07 -0400

    Update LAB#1.swift

--- Switf/Lab/LAB#1.swift
+++ Switf/Lab/LAB#1.swift
@@ -1,15 +1,9 @@
 import SwiftUI
 
-
-
 struct ContentView: View {
-
     var body: some View {
-
         ZStack{
-
             Color.black
-
             .edgesIgnoringSafeArea(.all)
 
             VStack{
