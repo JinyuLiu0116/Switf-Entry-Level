4e3656a48510a5c7b3f3f2945946085e926b82bb
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-04-04 20:52:01 -0400

    Update practice4.swift

--- Switf/practice4.swift
+++ Switf/practice4.swift
@@ -1,1 +1,28 @@
+import SwiftUI
+struct ContentView: View {
+    @State private var isClick = false
+    var body: some View {
+        NavigationView{
+            VStack{
+            Text("You are going to flip a coin - do you want to choose heads or tails?")
+            NavigationLink(destination: Text("I am a heard")){
+                Text("chooes heard")
+            }
+            Spacer()
+                .frame(height:10)
+            NavigationLink(destination: Text("I am a tail")){
+                Text("chooes tail")
+            }
+                
+            }
+            .navigationBarTitle("I am a navigation")
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
 
