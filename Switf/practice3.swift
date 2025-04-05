b72a435d01a2b032e20329d8f9f195ed55ecf5e6
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-04-04 20:18:50 -0400

    Update practice3.swift

--- Switf/practice3.swift
+++ Switf/practice3.swift
@@ -1,1 +1,23 @@
+import SwiftUI
 
+struct ContentView: View{
+@State private var isClick = false
+    var body: some View{
+        VStack{
+            Button(action:{
+                self.isClick.toggle()
+            }){
+                Text("Click me")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .background(isClick ? LinearGradient(gradient: Gradient(colors:[Color.red,Color.yellow,Color.green]),startPoint: .leading, endPoint: .trailing): LinearGradient(gradient: Gradient(colors:[Color.green,Color,yellow,Color.red]),startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.purple, lineWidth: 8))
+            }
+        }
+        
+    }
+}
