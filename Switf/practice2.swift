4c396c8885dd1265af3d34d296d25398ae7bb157
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-04-04 20:02:02 -0400

    Update practice2.swift

--- Switf/practice2.swift
+++ Switf/practice2.swift
@@ -1,1 +1,24 @@
+import SwiftUI
 
+struct ContentView: View{
+    var body: some View{
+        VStack{
+            Button(action:{
+                print("Hello World")
+                
+            }){
+                Text("I am a button")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .background(Color.purple)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.purple, lineWidth:8)
+                    )
+            }
+        }
+    }
+}
