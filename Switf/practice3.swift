7ee1cfee81ad43876921a6d744a53f07ea305814
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-04-04 20:20:08 -0400

    Update practice3.swift

--- Switf/practice3.swift
+++ Switf/practice3.swift
@@ -10,7 +10,10 @@ struct ContentView: View{
                 Text("Click me")
                     .fontWeight(.bold)
                     .font(.title)
-                    .background(isClick ? LinearGradient(gradient: Gradient(colors:[Color.red,Color.yellow,Color.green]),startPoint: .leading, endPoint: .trailing): LinearGradient(gradient: Gradient(colors:[Color.green,Color,yellow,Color.red]),startPoint: .leading, endPoint: .trailing))
+                    .background(isClick ? LinearGradient(gradient: Gradient(colors:[Color.red,Color.yellow,Color.green]),
+                                            startPoint: .leading, endPoint: .trailing): 
+                                          LinearGradient(gradient: Gradient(colors:[Color.green,Color,yellow,Color.red]),
+                                            startPoint: .leading, endPoint: .trailing))
                     .cornerRadius(10)
                     .foregroundColor(.white)
                     .padding(10)
