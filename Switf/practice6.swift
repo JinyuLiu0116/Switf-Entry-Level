05233b04b3cd7344dab5bb553005ed699ac6f7f4
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-04-25 22:45:10 -0400

    Update practice6.swift

--- Switf/practice6.swift
+++ Switf/practice6.swift
@@ -1,1 +1,72 @@
+import SwiftUI
+struct ContentView: View {
+    @State private var isÇlick = false
+    var body: some View {
+        NavigationView{
+            VStack{
+                    Image("images-3")
+                        .frame(width:100,height:100)
+                    Text("Welcome")
+                    Spacer()
+                        .frame(height:20)
+                    Text("Student Portal")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                        .frame(height:60)
+                    HStack{
+                        VStack{
+                            Text("ABC")
+                            Spacer()
+                            .frame(height:20)
+                            Button(action: {
+                            }) {
+                                Text("Logn in")
+                                    .foregroundColor(.white)
+                            }
+                            .background(Color.blue)
+                            .cornerRadius(8)
+                        }
+                        
+                        Rectangle()
+                            .frame(width:200, height:1)
+                            .foregroundColor(.white)
+                        
+                        VStack{
+                            Text("ABC")
+                            Spacer()
+                            .frame(height:20)
+                            Button(action: {
+                            }) {
+                                Text("Logn in")
+                                    .foregroundColor(.white)
+                            }
+                            .background(Color.blue)
+                            .cornerRadius(8)
+                        }
+                    
+                    }
+               
+                        
+                    
+                
+                }
+            NavigationLink(destination:(Text("hellow")){
+                HStack{
+                Text("Have An Account?")
+                Spacer()
+                    .frame(width:30)
+                Text("sign in")
+            }
+            }
+    
+        }
+        
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
